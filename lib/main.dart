015a11a05a0acddee2395a0680a5c0a60718649c
Pedro Mauricio Echeverry Rey <pedromauricioecheverry@gmail.com>
2020-01-19 20:16:35 +0100

    Ajustes para interactuar con rutas desde archivo .json

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-// import 'package:componentes/src/pages/home_temp.dart';
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/alert_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,8 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Componentes APP',
-        debugShowCheckedModeBanner: false,
-        home: HomePage());
+      title: 'Componentes APP',
+      debugShowCheckedModeBanner: false,
+      // home: HomePage()),
+      routes: getAppliationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        print('Ruta llamada: ${settings.name}');
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
+      },
+    );
   }
 }
