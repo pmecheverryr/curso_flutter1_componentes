c58ed9fd317afca0cb04a873a02eb4b87a42f536
Pedro Mauricio Echeverry Rey <pedromauricioecheverry@gmail.com>
2020-01-19 20:18:23 +0100

    Creacion de paginas alert, avatar y card

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[_cartTipo1()],
+      ),
+    );
+  }
+
+  Widget _cartTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text("Soy el titulo de esta tarjeta"),
+            subtitle: Text(
+                "Ea aliqua nisi esse in occaecat eiusmod amet ut consectetur ullamco consectetur Lorem est. Eu enim Lorem aute reprehenderit ullamco anim excepteur."),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
