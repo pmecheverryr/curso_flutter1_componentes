5f15706bb77349f5578f39d56d1263ef710e766c
Pedro Mauricio Echeverry Rey <pedromauricioecheverry@gmail.com>
2020-01-20 21:48:34 +0100

    Ajustes pagina card, ejercicios con Card, Container, BoxShadow

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,13 +9,21 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: <Widget>[_cartTipo1()],
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2()
+        ],
       ),
     );
   }
 
-  Widget _cartTipo1() {
+  Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -38,6 +46,48 @@ class CardPage extends StatelessWidget {
             ],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    final card = Container(
+      // clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage(
+                'http://hdwarena.com/wp-content/uploads/2018/05/Best-Landscape.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          // Image(
+          //   image: NetworkImage(
+          //       'http://hdwarena.com/wp-content/uploads/2018/05/Best-Landscape.jpg'),
+          // ),
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('No tengo idea de que poner')),
+        ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.white,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+                spreadRadius: 2.0,
+                offset: Offset(2.0, 10.0))
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
       ),
     );
   }
